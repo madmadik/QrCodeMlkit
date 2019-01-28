18d8b97c93437a993f5e829c365eea32f4c0baea
Madik <madik404error@gmail.com>
2019-01-28 14:29:13 +0600

    Refactor BarcodeProcessor

--- app/src/main/java/com/example/madik/barcodesenimdetector/BarcodeRecognitionProcessor.kt
+++ app/src/main/java/com/example/madik/barcodesenimdetector/BarcodeRecognitionProcessor.kt
@@ -9,11 +9,31 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import java.io.IOException
 import java.util.concurrent.atomic.AtomicBoolean
 import android.graphics.Bitmap
+import com.google.android.gms.vision.barcode.Barcode.*
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode.*
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions
 
 class BarcodeRecognitionProcessor {
     private val TAG = "BarcodeRecProc"
 
-    private var detector = FirebaseVision.getInstance().visionBarcodeDetector
+    val options = FirebaseVisionBarcodeDetectorOptions.Builder()
+        .setBarcodeFormats(
+            FORMAT_QR_CODE,
+            AZTEC,
+            CODABAR,
+            CODE_39,
+            CODE_93,
+            CODE_128,
+            DATA_MATRIX,
+            EAN_8,
+            EAN_13,
+            ITF,
+            UPC_A,
+            UPC_E
+        )
+        .build()
+
+    private var detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options)
 
     private val shouldThrottle = AtomicBoolean(false)
 
@@ -33,12 +53,16 @@ class BarcodeRecognitionProcessor {
             return
         }
 
+        val width = data.width
+        val height = data.height
+        val viewFinderWidth = (width * viewFinderWidthFraction).toInt()
+
         val croppedBitmap = Bitmap.createBitmap(
             data,
-            ((data.width - data.width * viewFinderWidth) / 2).toInt(),
-            ((data.height - data.width * viewFinderWidth) / 2).toInt(),
-            (data.width * viewFinderWidth).toInt(),
-            (data.width * viewFinderWidth).toInt()
+            (width - viewFinderWidth) / 2,
+            (height - viewFinderWidth) / 2,
+            viewFinderWidth,
+            viewFinderWidth
         )
 
         val image = FirebaseVisionImage.fromBitmap(croppedBitmap)
@@ -86,4 +110,4 @@ class BarcodeRecognitionProcessor {
     }
 }
 
-private const val viewFinderWidth = 0.625
+private const val viewFinderWidthFraction = 0.625
